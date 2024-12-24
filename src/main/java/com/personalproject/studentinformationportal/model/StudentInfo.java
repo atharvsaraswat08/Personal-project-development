fefa04bd78124177220b7a58df96bcf880cc04a0
atharv saraswat <atharv.saraswat08@gmail.com>
2024-12-24 22:21:08 +0530

    minor update

--- src/main/java/com/personalproject/studentinformationportal/model/StudentInfo.java
+++ src/main/java/com/personalproject/studentinformationportal/model/StudentInfo.java
@@ -12,6 +12,42 @@ public class StudentInfo {
 	private String motherName;
 	private String address;
 	private int id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getMotherName() {
+		return motherName;
+	}
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 	
 }
